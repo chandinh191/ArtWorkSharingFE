dba1bf7b5dd032a816e3d39eb908046d475d69c0
chandinh191 <vinhtc191@gmail.com>
2024-03-01 16:05:25 +0700

    header

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,151 +1,96 @@
 import classNames from 'classnames/bind';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faCircleQuestion,
-    faCoins,
-    faEarthAsia,
-    faEllipsisVertical,
-    faGear,
-    faKeyboard,
-    faSignOut,
-    faUser,
-} from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
-import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css';
 
-import config from '~/config';
-import Button from '~/components/Button';
 import styles from './Header.module.scss';
-import images from '~/assets/images';
-import Menu from '~/components/Popper/Menu';
-import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
-import Image from '~/components/Image';
-import Search from '../Search';
-
 const cx = classNames.bind(styles);
 
-const MENU_ITEMS = [
-    {
-        icon: <FontAwesomeIcon icon={faEarthAsia} />,
-        title: 'English',
-        children: {
-            title: 'Language',
-            data: [
-                {
-                    type: 'language',
-                    code: 'en',
-                    title: 'English',
-                },
-                {
-                    type: 'language',
-                    code: 'vi',
-                    title: 'Tiếng Việt',
-                },
-            ],
-        },
-    },
-    {
-        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
-        title: 'Feedback and help',
-        to: '/feedback',
-    },
-    {
-        icon: <FontAwesomeIcon icon={faKeyboard} />,
-        title: 'Keyboard shortcuts',
-    },
-];
-
 function Header() {
-    const currentUser = true;
-
-    // Handle logic
-    const handleMenuChange = (menuItem) => {
-        switch (menuItem.type) {
-            case 'language':
-                // Handle change language
-                break;
-            default:
-        }
-    };
-
-    const userMenu = [
-        {
-            icon: <FontAwesomeIcon icon={faUser} />,
-            title: 'View profile',
-            to: '/@hoaa',
-        },
-        {
-            icon: <FontAwesomeIcon icon={faCoins} />,
-            title: 'Get coins',
-            to: '/coin',
-        },
-        {
-            icon: <FontAwesomeIcon icon={faGear} />,
-            title: 'Settings',
-            to: '/settings',
-        },
-        ...MENU_ITEMS,
-        {
-            icon: <FontAwesomeIcon icon={faSignOut} />,
-            title: 'Log out',
-            to: '/logout',
-            separate: true,
-        },
-    ];
-
     return (
-        <header className={cx('wrapper')}>
-            <div className={cx('inner')}>
-                <Link to={config.routes.home} className={cx('logo-link')}>
-                    <img src={images.logo} alt="Tiktok" />
-                </Link>
-
-                <Search />
-
-                <div className={cx('actions')}>
-                    {currentUser ? (
-                        <>
-                            <Tippy delay={[0, 50]} content="Upload video" placement="bottom">
-                                <button className={cx('action-btn')}>
-                                    <UploadIcon />
-                                </button>
-                            </Tippy>
-                            <Tippy delay={[0, 50]} content="Message" placement="bottom">
-                                <button className={cx('action-btn')}>
-                                    <MessageIcon />
-                                </button>
-                            </Tippy>
-                            <Tippy delay={[0, 50]} content="Inbox" placement="bottom">
-                                <button className={cx('action-btn')}>
-                                    <InboxIcon />
-                                    <span className={cx('badge')}>12</span>
-                                </button>
-                            </Tippy>
-                        </>
-                    ) : (
-                        <>
-                            <Button text>Upload</Button>
-                            <Button primary>Log in</Button>
-                        </>
-                    )}
-
-                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
-                        {currentUser ? (
-                            <Image
-                                className={cx('user-avatar')}
-                                src="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
-                                alt="Nguyen Van A"
-                            />
-                        ) : (
-                            <button className={cx('more-btn')}>
-                                <FontAwesomeIcon icon={faEllipsisVertical} />
-                            </button>
-                        )}
-                    </Menu>
+        <>
+            {/* Header Section Begin */}
+            <header className={cx('header')}>
+                <div className={cx('container-fluid')}>
+                    <div className={cx('row')}>
+                        <div className={cx('col-xl-3 col-lg-2')}>
+                            <div className={cx('header__logo')}>
+                                <a href="./index.html">
+                                    <img src="img/logo.png" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <div className={cx('col-xl-6 col-lg-7')}>
+                            <nav className={cx('header__menu')}>
+                                <ul>
+                                    <li className={cx('active')}>
+                                        <a href="./index.html">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Women’s</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Men’s</a>
+                                    </li>
+                                    <li>
+                                        <a href="./shop.html">Shop</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Pages</a>
+                                        <ul className={cx('dropdown')}>
+                                            <li>
+                                                <a href="./product-details.html">Product Details</a>
+                                            </li>
+                                            <li>
+                                                <a href="./shop-cart.html">Shop Cart</a>
+                                            </li>
+                                            <li>
+                                                <a href="./checkout.html">Checkout</a>
+                                            </li>
+                                            <li>
+                                                <a href="./blog-details.html">Blog Details</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="./blog.html">Blog</a>
+                                    </li>
+                                    <li>
+                                        <a href="./contact.html">Contact</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className={cx('col-lg-3')}>
+                            <div className={cx('header__right')}>
+                                <div className={cx('header__right__auth')}>
+                                    <a href="#">Login</a>
+                                    <a href="#">Register</a>
+                                </div>
+                                <ul className={cx('header__right__widget')}>
+                                    <li>
+                                        <span className={cx('icon_search search-switch')} />
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <span className={cx('icon_heart_alt')} />
+                                            <div className={cx('tip')}>2</div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <span className={cx('icon_bag_alt')} />
+                                            <div className={cx('tip')}>2</div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx('canvas__open')}>
+                        <i className={cx('fa fa-bars')} />
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+            {/* Header Section End */}
+        </>
     );
 }
 
