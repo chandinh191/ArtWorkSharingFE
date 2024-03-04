cf6d3d2cb3835f6476cd79bdf6151fb6e69d20aa
zimbear3103 <kiendhduc@gmail.com>
2024-03-04 20:25:36 +0700

    add view cart, ProductDetail, check out

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,7 +4,9 @@ const routes = {
     profile: '/@:nickname',
     upload: '/upload',
     search: '/search',
-    live: '/live',
+    productDetail: '/ProductDetail',
+    cart: '/Cart',
+    checkout: '/CheckOut',
 };
 
 export default routes;
