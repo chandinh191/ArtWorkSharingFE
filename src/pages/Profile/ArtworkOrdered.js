255f31a302fc0e9b5811de37a59f91f88c5d07dd
chandinh191 <vinhtc191@gmail.com>
2024-03-26 17:15:12 +0700

    .

--- src/pages/Profile/ArtworkOrdered.js
+++ src/pages/Profile/ArtworkOrdered.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import appsetting from '../../appsetting.json';
 const { SERVER_API } = appsetting;
 function ArtworkOrdered() {
+    const token = localStorage.getItem('token');
+    const useridlocal = localStorage.getItem('userid');
     const [artworks, setArtworks] = useState([]);
     useEffect(() => {
         // Make the API request
@@ -18,24 +20,40 @@ function ArtworkOrdered() {
                 console.error('Error fetching data:', error);
             });
     }, []);
-    /* Lấy user tạm ********************** */
-    const userid = '871a809a-b3fa-495b-9cc2-c5d738a866cf';
-    const myArtworks = artworks.filter((artwork) => artwork.userOwnerId === userid);
+    const status = 1;
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const res = await fetch(`${SERVER_API}/Order/GetOrderByStatus?status=${status}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                if (res.ok) {
+                    const resData = await res.json(); // Extract JSON data from response
+                    setOrders(resData);
+                } else {
+                    console.error('Failed to fetch artwork');
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchOrders();
+    }, [status]);
 
-    const userArtworks = myArtworks.filter((artwork) => {
-        if (artwork.orders && Array.isArray(artwork.orders)) {
-            // Nếu có các đơn hàng cho tác phẩm
-            return artwork.orders.some((order) => order.status === 1);
-        } else if (artwork.orders && typeof artwork.orders === 'object') {
-            // Nếu chỉ có một đơn hàng cho tác phẩm
-            return artwork.orders.status === 1;
-        }
-        return false; // Nếu không có đơn hàng nào cho tác phẩm
-    });
+    console.log(orders);
+
+    /* Lấy user */
+    const userid = useridlocal;
+    const myArtworks = artworks.filter((artwork) => artwork.userOwnerId === userid);
 
     return (
         <div>
-            {userArtworks.map((artwork, index) => (
+            {myArtworks.map((artwork, index) => (
                 <div className="row">
                     <div className="product-card-horizontal">
                         <img src={artwork.imageUrl} alt="Product Image" className="product-image" />
