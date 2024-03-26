0271a7483fca2683cc05c016610400611968389f
zimbear3103 <kiendhduc@gmail.com>
2024-03-26 21:09:40 +0700

    add cloudinary to create url image

--- src/pages/Profile/Create.js
+++ src/pages/Profile/Create.js
@@ -4,12 +4,16 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPalette } from '@fortawesome/free-solid-svg-icons';
+import { Cloudinary } from "@cloudinary/url-gen";
+
 import appsetting from '../../appsetting.json';
 const { SERVER_API } = appsetting;
 
 function CreateArtWork() {
-    const [category, setCategories] = useState([]);
+    const cld = new Cloudinary({ cloud: { cloudName: 'doatgdqwk' } });
+    const userId = localStorage.getItem('userid');
     const [selectedFile, setSelectedFile] = useState(null);
+    const [imageUrl, setImageUrl] = useState(null);
     const handleUploadClick = () => {
         document.getElementById('fileInput').click();
     };
@@ -17,133 +21,245 @@ function CreateArtWork() {
         name: '',
         description: '',
         categoryId: '',
-        expireDate: '',
-        stock: '',
+        price: Number,
     });
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         setSelectedFile(file);
     };
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const formDataWithFile = new FormData();
-        formDataWithFile.append('name', formData.name);
-        formDataWithFile.append('description', formData.description);
-        formDataWithFile.append('category', formData.categoryId);
-        formDataWithFile.append('price', formData.price);
-        formDataWithFile.append('image', selectedFile);
 
-        axios
-            .post('https://localhost:7178/api/Category/Add', formDataWithFile)
-            .then((response) => {
-                console.log('Response:', response.data);
-                // Do something with the response if needed
-            })
-            .catch((error) => {
-                console.error('Error posting data:', error);
+        // Upload the image to Cloudinary
+        if (!selectedFile) return;
+
+        // Create a formData object to send to Cloudinary
+        const imageData = new FormData();
+        imageData.append('file', selectedFile);
+        imageData.append('upload_preset', preset_key);
+
+        // Send the image to Cloudinary
+        try {
+            const response = await fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
+                method: 'POST',
+                body: imageData,
             });
+
+            const data = await response.json();
+
+            console.log(data.secure_url);
+            // Set the uploaded image URL
+            if (data.secure_url) {
+                try {
+                    // Create the requestBody object with the imageUrl
+                    const requestBody = JSON.stringify({
+                        userAccountId: userId,
+                        userOwnerId: userId,
+                        categoryId: formData.categoryId,
+                        name: formData.name,
+                        description: formData.description,
+                        price: formData.price,
+                        imageUrl: data.secure_url, // Use the Cloudinary image URL here
+                    });
+
+                    console.log(requestBody); // Log the body before making the fetch call
+
+                    // Make the fetch call
+                    const res = await fetch(`${SERVER_API}/Artwork/Add`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: requestBody,
+                    });
+
+                    if (res.ok) {
+                        window.location.reload(); // Reload the page if the request is successful
+                    } else {
+                        console.error('Error:', res.statusText); // Log error message if request fails
+                    }
+                } catch (error) {
+                    console.error('Error posting data:', error); // Log error if fetch fails
+                }
+            } else {
+                console.error('Image URL is not available.'); // Log error if imageUrl is not available
+            }
+        } catch (error) {
+            console.error('Error uploading image:', error);
+        }
+        console.log(imageUrl + "testttttt");
+        // Ensure imageUrl is available before proceeding
+        // Ensure imageUrl is available
+
     };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const preset_key = "df8pqawi";
+    const cloud_name = "doatgdqwk";
+    const handleImageUpload = async () => {
+        // Check if a file was selected
+        if (!selectedFile) return;
+
+        // Create a formData object to send to Cloudinary
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+        formData.append('upload_preset', preset_key);
+
+        // Send the image to Cloudinary
+        try {
+            const response = await fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
+                method: 'POST',
+                body: formData,
+            });
+
+            const data = await response.json();
+
+            console.log(data.secure_url);
+            // Set the uploaded image URL
+            setImageUrl(data.secure_url);
+        } catch (error) {
+            console.error('Error uploading image:', error);
+        }
+    };
+    const [categories, setCategories] = useState([]);
     useEffect(() => {
-        // Make the API request
-        axios
-            .get(`https://localhost:7178/api/Category/GetAll`)
-            .then((response) => {
+        const fetchData = async () => {
+            try {
+                // Make the API request
+                const response = await axios.get(`${SERVER_API}/Category/GetAll`);
                 // Update the state with the fetched data
                 setCategories(response.data);
-            })
-            .catch((error) => {
+            } catch (error) {
                 // Handle any errors here
                 console.error('Error fetching data:', error);
-            });
+            }
+        };
+
+        fetchData(); // Call the async function
     }, []);
 
     return (
         <>
-            <div className="tm-bg-primary-dark tm-block tm-block-h-auto">
-                <div className="row">
-                    <div className="col-12">
-                        <h2 className="tm-block-title d-inline-block">Add Product</h2>
-                    </div>
-                </div>
-                <div className="row tm-edit-product-row">
-                    <div className="col-xl-6 col-lg-6 col-md-12">
-                        <form onSubmit={handleSubmit} className="tm-edit-product-form">
-                            <div className="form-group mb-3">
-                                <label htmlFor="name">Art Work Name</label>
-                                <input
-                                    id="name"
-                                    name="name"
-                                    type="text"
-                                    className="form-control validate"
-                                    required=""
-                                />
+            <section className="shop spad">
+                <div className="container" style={{ padding: '30px 15px' }}>
+                    <div className="tm-bg-primary-dark tm-block tm-block-h-auto">
+                        <div className="row">
+                            <div className="col-12">
+                                <h2 className="tm-block-title d-inline-block">Add Product</h2>
                             </div>
-                            <div className="form-group mb-3">
-                                <label htmlFor="description">Description</label>
-                                <textarea className="form-control validate" rows={3} required="" defaultValue={''} />
+                        </div>
+                        <form onSubmit={handleSubmit} className="row tm-edit-product-row">
+                            <div className="col-xl-6 col-lg-6 col-md-12">
+                                <div className="tm-edit-product-form">
+                                    <div className="form-group mb-3">
+                                        <input
+                                            id="name"
+                                            name="name"
+                                            type="text"
+                                            placeholder='Art Work Name'
+                                            className="form-control validate"
+                                            required=""
+                                            value={formData.name}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <textarea
+                                            id="description"
+                                            name="description"
+                                            className="form-control validate"
+                                            rows={3}
+                                            placeholder='Description'
+                                            required=""
+                                            defaultValue={''}
+                                            value={formData.description}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <select
+                                            className="custom-select tm-select-accounts"
+                                            id="categoryId"
+                                            name="categoryId"
+                                            value={formData.categoryId}
+                                            onChange={handleChange}
+                                        >
+                                            <option value="">Select category</option>
+                                            {categories.map((category, i) => (
+                                                <option key={i} value={category.id}>
+                                                    {category.categoryName}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <div className="row">
+                                        <div className="form-group mb-3 col-xs-12 col-sm-6">
+                                            <input
+                                                id="price"
+                                                name="price"
+                                                type="number"
+                                                placeholder='price'
+                                                min={0}
+                                                className="form-control validate"
+                                                data-large-mode="true"
+                                                value={formData.price}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                        <div className="form-group mb-3 col-xs-12 col-sm-6">
+                                            <label htmlFor="stock">Units In Stock</label>
+                                            <input
+                                                id="stock"
+                                                name="stock"
+                                                type="text"
+                                                className="form-control validate"
+                                                required=""
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="form-group mb-3">
-                                <label htmlFor="category">Category</label>
-                                <select className="custom-select tm-select-accounts" id="category">
-                                    <option selected="">Select category</option>
-                                    {category.forEach(function (category, i) {
-                                        <option value={category.categoryId}>{category.name}</option>;
-                                    })}
-                                </select>
-                            </div>
-                            <div className="row">
-                                <div className="form-group mb-3 col-xs-12 col-sm-6">
-                                    <label htmlFor="expire_date">Price</label>
+                            <div className="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
+                                <div className="tm-product-img-dummy mx-auto">
+                                    {selectedFile && (
+                                        <img
+                                            src={URL.createObjectURL(selectedFile)}
+                                            alt="Product Image"
+                                            className="img-fluid"
+                                            style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+                                        />
+                                    )}
+                                </div>
+                                <div className="custom-file mt-3 mb-3">
                                     <input
-                                        id="price"
-                                        name="price"
-                                        type="number"
-                                        min={0}
-                                        className="form-control validate"
-                                        data-large-mode="true"
+                                        id="fileInput"
+                                        type="file"
+                                        style={{ display: 'none' }}
+                                        onChange={handleFileChange}
+                                    />
+                                    <input
+                                        type="button"
+                                        className="btn btn-primary btn-block mx-auto"
+                                        value="Upload image"
+                                        onClick={handleUploadClick}
                                     />
                                 </div>
-                                <div className="form-group mb-3 col-xs-12 col-sm-6">
-                                    <label htmlFor="stock">Units In Stock</label>
-                                    <input
-                                        id="stock"
-                                        name="stock"
-                                        type="text"
-                                        className="form-control validate"
-                                        required=""
-                                    />
-                                </div>
+                            </div>
+                            <div className="col-12">
+                                <button type="submit" className="btn btn-primary btn-block text-uppercase">
+                                    Create
+                                </button>
                             </div>
                         </form>
                     </div>
-                    <div className="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
-                        <div className="tm-product-img-dummy mx-auto">
-                            {selectedFile && (
-                                <img
-                                    src={URL.createObjectURL(selectedFile)}
-                                    alt="Product Image"
-                                    className="img-fluid"
-                                    style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-                                />
-                            )}
-                        </div>
-                        <div className="custom-file mt-3 mb-3">
-                            <input id="fileInput" type="file" style={{ display: 'none' }} onChange={handleFileChange} />
-                            <input
-                                type="button"
-                                className="btn btn-primary btn-block mx-auto"
-                                value="Upload image"
-                                onClick={handleUploadClick}
-                            />
-                        </div>
-                    </div>
-                    <div className="col-12">
-                        <button type="submit" className="btn btn-primary btn-block text-uppercase">
-                            Create
-                        </button>
-                    </div>
                 </div>
-            </div>
+            </section>
         </>
     );
 }
